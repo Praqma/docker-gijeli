e172f84525a214bfc1139ce2835631c37d44dba5
ali <ali@praqma.net>
2015-10-14 17:30:31 +0200

    bug fxd

--- entry.sh
+++ entry.sh
@@ -18,7 +18,7 @@ elif [ "${url}" == "check" ]
 	then
 jekyll serve --force_polling -B -H 0.0.0.0
 echo ¤¤¤¤¤¤¤¤ Validating, be Patient ¤¤¤¤¤¤¤¤
-linkchecker --check-css --check-html --complete --anchors --quiet -F=html/report/site_report.html http://0.0.0.0:4000
+linkchecker --check-css --check-html --complete --anchors -F=text/site_report.txt -F=html/report/site_report.html http://0.0.0.0:4000
 else
 exec	less -rf -FX /help.txt
 fi
